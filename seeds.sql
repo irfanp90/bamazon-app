8e572c2bcd9521bae502b433d1411fbad01f426b
irfanp90 <irfanp90@gmail.com>
2019-03-31 16:27:48 -0500

    update seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -3,5 +3,5 @@ USE bamazon;
 
 /* Insert inforamtion into table products */
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("NBA 2K19", "Video Game", 29.99, 25), ("Ray Ban Shades", "Eyewear", 129.99, 10), ("Nike AirForce Ones", "Footwear", 229.99, 10),("Levi's Jeans", "Clothing", 29.99, 50),("Avengers", "Movies", 19.99, 5), ("Thomas the Train set", "Toys", 11.99, 125),("The North Face T-shirt", "Clothing", 49.99, 75),
+VALUES ("NBA 2K19", "Video Game", 29.99, 25), ("Ray Ban Shades", "Eyewear", 129.99, 10), ("Nike AirForce Ones", "Footwear", 229.99, 10),("Levi's Jeans", "Clothing", 29.99, 50),("Avengers", "Movies", 19.99, 5), ("Thomas the Train set", "Toys", 11.99, 125),("The North Face T-shirt", "Clothing", 49.99, 75),("Samsung TV", "Electronics", 1000.00, 5),("Nike Socks", "Clothing", 4.99, 200), ("Tide Laundry Detergent", "Household item", 14.99, 10),("Toilet Paper 24 pack", "Household item", 1.99, 25), ("Toothbrush/toothpaste combo", "Household item", 4.99, 250),("Snickers", "Food", 0.75, 25),("Dyson vacuum", "Household item", 329.99, 2),("Pencil", "School Supplies", 0.10, 1125),
 
