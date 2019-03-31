3645ce999918cecbd516680ba8fc94c9781bcb5c
irfanp90 <irfanp90@gmail.com>
2019-03-31 17:35:29 -0500

    .gitignore and js files update

---
+++ bamazonCustomer.js
@@ -0,0 +1,47 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+//connecting to My SQL
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3307,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  showTable();
+});
+//show user data from the products table
+function showTable() {
+  connection.query("SELECT * FROM products", function(err, response) {
+    if (err) throw err;
+    // console.log(response);
+    for (var i = 0; i < response.length; i++) {
+      console.log(
+        "Item ID: " +
+          response[i].item_id +
+          " | " +
+          "Product: " +
+          response[i].product_name +
+          " | " +
+          "Department: " +
+          response[i].department_name +
+          " | " +
+          "Price: " +
+          response[i].price +
+          " | " +
+          "Stock-quantity: " +
+          response[i].stock_quantity
+      );
+    }
+  });
+}
